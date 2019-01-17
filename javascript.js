b790708184e1eeaaa964761c3756624c135ebc14
Haytham Choi <keun1996@gmail.com>
2019-01-17 13:00:14 +0900

    final revisory

--- javascript.js
+++ javascript.js
@@ -1,4 +1,4 @@
-<script>
+
   $(window).on("scroll", function() {
     if$(window).scrollTop() {
       $('nav').addClass('black');
@@ -7,4 +7,12 @@
       $('nav').removeClass('black');
     }
   });
-</script>
+
+
+function initmap() {
+  var map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat:52.5206, lng: 13.4098},
+    scrollwheel: false,
+    zoom:8
+  });
+};
