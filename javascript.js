2a75e997e9669188081e8a7d5475d1ecad2939ee
Haytham Choi <keun1996@gmail.com>
2019-01-17 15:59:55 +0900

    final revisory

--- javascript.js
+++ javascript.js
@@ -9,10 +9,17 @@
   });
 
 
-function initmap() {
-  var map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat:52.5206, lng: 13.4098},
-    scrollwheel: false,
-    zoom:8
-  });
-};
+  <script>
+      train.onclick = function() {
+        let start = Date.now();
+
+        let timer = setInterval(function() {
+          let timePassed = Date.now() - start;
+
+          train.style.left = timePassed / 5 + 'px';
+
+          if (timePassed > 2000) clearInterval(timer);
+
+        }, 20);
+      }
+    </script>
